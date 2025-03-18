94736036a31d2376aa8428acb98601957b17f9ea
j1echeng <jiecheng.zjc@bitsun-inc.com>
2025-03-18 16:07:07 +0800

    feat: 数字滚动

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ScrollNumber from "@/component/scrollNumber.vue";
+import {ref} from "vue";
+
+const number = ref(0)
+const handleClick = () => {
+	number.value = Math.random() * Math.pow(10, Math.floor((Math.random() * 10)))
+}
+
+</script>
+
+<template>
+	<ScrollNumber :modelValue="number" @click="handleClick" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/component/scrollNumber.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { computed, ref, watch, nextTick } from "vue";
+
+const props = defineProps({
+  modelValue: { type: [Number, String], required: true },
+  duration: { type: Number, default: 1 }, // 动画持续时间（秒）
+  height: { type: Number, default: 30 },  // 数字高度
+  width: { type: Number, default: 10 }    // 数字宽度
+});
+
+const getStr = computed(() => {
+  return String(props.modelValue).split('');
+});
+
+const numRef = ref([]);
+
+// 执行数字滚动动画
+const animateNumbers = async () => {
+	// 等待DOM更新为0
+	await nextTick()
+	if (!numRef.value || numRef.value.length === 0) return;
+
+	numRef.value.forEach(item => {
+  	  if (!item) return;
+		item.style.transform = `translateY(0)`;
+		item.style.transitionDuration = `0s`;
+		setTimeout(() => {
+			const targetPosition = -(item.children.length - 1) * props.height;
+  	  		item.style.transform = `translateY(${targetPosition}px)`;
+  	  		item.style.transitionDuration = `${props.duration}s`;
+		}, 100)
+  	});
+};
+
+// 监听数值变化，触发动画
+watch(() => props.modelValue, () => {
+  animateNumbers();
+}, { immediate: true });
+</script>
+
+<template>
+  <div class="container" :style="{ height: `${props.height}px` }">
+    <div 
+      v-for="(item, idx) in getStr" 
+      :key="idx"
+      class="digit-container" 
+      :style="{ height: `${props.height}px` }"
+    >
+      <div
+        v-if="!isNaN(item)"
+        class="number-column"
+        ref="numRef"
+      >
+        <div 
+          v-for="index in Number(item) + 1" 
+          :key="index - 1"
+          class="digit"
+          :style="{ 
+            height: `${props.height}px`, 
+            width: `${props.width}px` 
+          }"
+        >
+          {{ index - 1 }}
+        </div>
+      </div>
+      <div 
+        v-else 
+        class="digit"
+        :style="{ 
+          height: `${props.height}px`, 
+          width: `${props.width}px` 
+        }"
+      >
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  overflow: hidden;
+}
+
+.digit-container {
+  overflow: hidden;
+}
+
+.number-column {
+  transform: translateY(0);
+  transition-timing-function: ease-in-out;
+}
+
+.digit {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
