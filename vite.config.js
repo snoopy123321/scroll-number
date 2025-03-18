99442a2d22316ab6efe8990175c69b1e9a9e0aaa
j1echeng <jiecheng.zjc@bitsun-inc.com>
2025-03-18 16:46:56 +0800

    chore:为了将静态资源部署在github服务器上

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scroll-number',
+  build:{
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     vueDevTools(),
